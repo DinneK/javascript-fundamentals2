8c11f49f6f551c41d4d22862fe1655fa485f8bc6
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-10 08:24:42 -0700

    Add medusa.js

--- mythical-creatures/exercises/medusa.js
+++ mythical-creatures/exercises/medusa.js
@@ -1,2 +1,24 @@
-var Person = require('./person');
-var Statue = require('./statue');
+var Person = require("./person");
+var Statue = require("./statue");
+
+class Medusa {
+  constructor(name) {
+    this.name = name;
+    this.statues = [];
+  }
+
+  gazeAtVictim(victim) {
+    let newStatue = new Statue(victim.name);
+    if (this.statues.length < 4) {
+      this.statues.push(newStatue);
+    }
+    if (this.statues.length > 3) {
+      let newPerson = new Person(this.statues[0].name);
+      this.statues.shift();
+      newPerson.mood = "relieved";
+      return newPerson;
+    }
+  }
+}
+
+module.exports = Medusa;
