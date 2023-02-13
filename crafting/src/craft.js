0a6760661edbaf30e1314c626f82f54a30a7fda5
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-13 16:03:21 -0700

    Add crafting.js

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,5 +1,25 @@
-class Craft {
+// var Material = require("./material");
 
+class Craft {
+  constructor(craftingInfo) {
+    this.name = craftingInfo.type;
+    this.materials = craftingInfo.materials;
+    this.completed = false;
+  }
+
+  completeCraft() {
+    this.completed = true;
+    return `All done! It looks great!`;
+  }
+
+  calculateProjectTotal() {
+    return this.materials
+      .map((material) => material.price * material.amount)
+      .reduce((acc, curr) => {
+        acc = acc + curr;
+        return acc;
+      });
+  }
 }
 
 module.exports = Craft;
