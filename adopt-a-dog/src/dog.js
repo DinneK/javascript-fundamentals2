4973908ee3b0f4f530ebadee301cbd6d25f720ec
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-17 23:21:38 -0700

    Add adopt-a-dog.js

--- adopt-a-dog/src/dog.js
+++ adopt-a-dog/src/dog.js
@@ -1,8 +1,40 @@
 class Dog {
-  constructor() {
-
+  constructor(dogInfo) {
+    this.name = dogInfo.name;
+    this.age = dogInfo.age;
+    this.energyLevel = 5;
+    this.hungry = true;
+    this.friends = [];
   }
 
+  eat() {
+    if (this.hungry) {
+      this.hungry = false;
+      return `Yum!`;
+    }
+    if (!this.hungry) {
+      return `I refuse to eat.`;
+    }
+  }
+
+  fetchBall() {
+    if (this.energyLevel <= 3) {
+      return `Nah, I'm going to sleep instead.`;
+    }
+    this.energyLevel--;
+    return `This is fun!`;
+  }
+
+  sleep() {
+    if (this.energyLevel >= 10) {
+      return `I have too much energy to rest. I'm going to chew something instead.`;
+    }
+    this.energyLevel++;
+  }
+
+  makeNewFriend(friend) {
+    this.friends.push(friend.name);
+  }
 }
 
 module.exports = Dog;
--- adopt-a-dog/src/person.js
+++ adopt-a-dog/src/person.js
@@ -1,10 +1,37 @@
 var Dog = require("./dog.js");
 
 class Person {
-  constructor() {
-
+  constructor(name, dogInfo) {
+    this.name = name;
+    this.dog = dogInfo;
   }
 
+  fillFoodBowl() {
+    this.dog.hungry = false;
+  }
+
+  throwBall() {
+    if (this.dog.energyLevel > 3) {
+      this.dog.energyLevel--;
+      return `${this.dog.name} loves playing fetch!`;
+    }
+  }
+
+  introduceNewFriends(dogFriend) {
+    this.dog.friends.push(dogFriend.name);
+  }
+
+  adoptAPup(dogName, dogAge) {
+    if (this.dog) {
+      return `You can't adopt ${dogName}. You already have ${this.dog.name}!`;
+    }
+    if (this.dog === undefined) {
+      this.dog = new Dog({
+        name: dogName,
+        age: dogAge,
+      });
+    }
+  }
 }
 
 module.exports = Person;
