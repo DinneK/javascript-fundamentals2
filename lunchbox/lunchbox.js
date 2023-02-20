d2310ea637203edecb8f46b572a4dc4e2eed5631
Dinne Kopelevich <dinne22@hotmail.com>
2023-02-19 22:54:50 -0700

    Add lunchbox.js

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,22 @@
 class LunchBox {
+  constructor(lunchBoxInfo) {
+    this.owner = lunchBoxInfo.owner;
+    this.material = lunchBoxInfo.madeOf;
+    this.shape = lunchBoxInfo.shape;
+    this.color = lunchBoxInfo.color;
+    this.snacks = [];
+  }
 
+  acquireSnack(snack) {
+    this.snacks.push(snack);
+    snack.isInLunchBox = true;
+  }
+
+  findHealthySnacks() {
+    return this.snacks
+      .filter((snack) => snack.type.toLowerCase().includes("fruit"))
+      .map((snack) => snack.type);
+  }
 }
 
 module.exports = LunchBox;
